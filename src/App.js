98f4f17f4799a3ceb59e9f9283afbe785b157d0b
Nguyendanghung0201 <dinhpv.11.96@gmail.com>
2021-01-29 21:52:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import RouteConfig from './router/RouterConfig';
 import { connect } from 'react-redux';
-import { authCheckState } from './store//actions/index';
+import { authCheckState } from './store/actions/index';
 import Loader from './components/UI/Loader/Loader';
-
 class App extends React.Component {
   componentWillMount() {
     this.props.onAuthCheckState()
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import postsReducer from './store/reducer/posts';
 import categoryReducer from './store/reducer/category';
